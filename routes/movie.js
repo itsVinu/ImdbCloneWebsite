fb96fbe40826716f8fb5abc361354e03dd0a5aff
itsVinu <vinaygpt12321@gmail.com>
2021-05-25 19:24:17 +0530

    API

--- routes/movie.js
+++ routes/movie.js
@@ -1,25 +1,29 @@
+if(process.env.NODE_ENV != 'production'){
+    require('dotenv').config()
+  }
+
 const express = require('express')
 const router = express.Router()
 const axios = require('axios')
 
 router.get('/movie',async(req,res)=>{
-    const upcoming_url = "https://api.themoviedb.org/3/movie/upcoming?api_key=3d812b53e8327e3496dbe05fb820cf0d"
+    const upcoming_url = `https://api.themoviedb.org/3/movie/upcoming?api_key=${process.env.API_KEY}`
     const uresponse = await axios.get(upcoming_url)
 
-    const popular_url = "https://api.themoviedb.org/3/movie/popular?api_key=3d812b53e8327e3496dbe05fb820cf0d"
+    const popular_url = `https://api.themoviedb.org/3/movie/popular?api_key=${process.env.API_KEY}`
     const presponse = await axios.get(popular_url)
 
-    const toprated_url = "https://api.themoviedb.org/3/movie/top_rated?api_key=3d812b53e8327e3496dbe05fb820cf0d"
+    const toprated_url = `https://api.themoviedb.org/3/movie/top_rated?api_key=${process.env.API_KEY}`
     const topresponse = await axios.get(toprated_url)
     
-    const nowplaying_url = "https://api.themoviedb.org/3/movie/now_playing?api_key=3d812b53e8327e3496dbe05fb820cf0d"
+    const nowplaying_url = `https://api.themoviedb.org/3/movie/now_playing?api_key=${process.env.API_KEY}`
     const npresponse = await axios.get(nowplaying_url)
 
     res.render('movies',{ures : uresponse, pres : presponse, topres : topresponse, npres : npresponse})
 })
 
 router.get('/movie/upcoming/all',async(req,res)=>{
-    const upcoming_url = "https://api.themoviedb.org/3/movie/upcoming?api_key=3d812b53e8327e3496dbe05fb820cf0d"
+    const upcoming_url = `https://api.themoviedb.org/3/movie/upcoming?api_key=${process.env.API_KEY}`
     const uresponse = await axios.get(upcoming_url)
 
     const title = "Upcoming Movies"
@@ -27,7 +31,7 @@ router.get('/movie/upcoming/all',async(req,res)=>{
 
 })
 router.get('/movie/top_rated/all',async(req,res)=>{
-    const toprated_url = "https://api.themoviedb.org/3/movie/top_rated?api_key=3d812b53e8327e3496dbe05fb820cf0d"
+    const toprated_url = `https://api.themoviedb.org/3/movie/top_rated?api_key=${process.env.API_KEY}`
     const topresponse = await axios.get(toprated_url)
 
     const title = "Top Rated Movies"
@@ -35,7 +39,7 @@ router.get('/movie/top_rated/all',async(req,res)=>{
     
 })
 router.get('/movie/popular/all',async(req,res)=>{
-    const popular_url = "https://api.themoviedb.org/3/movie/popular?api_key=3d812b53e8327e3496dbe05fb820cf0d"
+    const popular_url = `https://api.themoviedb.org/3/movie/popular?api_key=${process.env.API_KEY}`
     const presponse = await axios.get(popular_url)
 
     const title = "Popular Movie"
@@ -43,7 +47,7 @@ router.get('/movie/popular/all',async(req,res)=>{
 
 })
 router.get('/movie/now_playing/all',async(req,res)=>{
-    const nowplaying_url = "https://api.themoviedb.org/3/movie/now_playing?api_key=3d812b53e8327e3496dbe05fb820cf0d"
+    const nowplaying_url = `https://api.themoviedb.org/3/movie/now_playing?api_key=${process.env.API_KEY}`
     const npresponse = await axios.get(nowplaying_url)
 
     const title = "Now Playing Movie"
--- routes/tvshows.js
+++ routes/tvshows.js
@@ -1,7 +1,6 @@
-const airing_today_url = "https://api.themoviedb.org/3/tv/airing_today?api_key=3d812b53e8327e3496dbe05fb820cf0d"
-const on_the_air_url = "https://api.themoviedb.org/3/tv/on_the_air?api_key=3d812b53e8327e3496dbe05fb820cf0d"
-const top_rated_url = "https://api.themoviedb.org/3/tv/top_rated?api_key=3d812b53e8327e3496dbe05fb820cf0d"
-const populartv_url = "https://api.themoviedb.org/3/tv/popular?api_key=3d812b53e8327e3496dbe05fb820cf0d"
+if(process.env.NODE_ENV != 'production'){
+    require('dotenv').config()
+  }
 
 const express = require('express')
 const router = express.Router()
@@ -9,10 +8,10 @@ const axios = require('axios')
 
 
 router.get('/tvshows',async(req,res)=>{
-    const airing_today_url = "https://api.themoviedb.org/3/tv/airing_today?api_key=3d812b53e8327e3496dbe05fb820cf0d"
-    const on_the_air_url = "https://api.themoviedb.org/3/tv/on_the_air?api_key=3d812b53e8327e3496dbe05fb820cf0d"
-    const top_rated_url = "https://api.themoviedb.org/3/tv/top_rated?api_key=3d812b53e8327e3496dbe05fb820cf0d"
-    const populartv_url = "https://api.themoviedb.org/3/tv/popular?api_key=3d812b53e8327e3496dbe05fb820cf0d"
+    const airing_today_url = `https://api.themoviedb.org/3/tv/airing_today?api_key=${process.env.API_KEY}`
+    const on_the_air_url = `https://api.themoviedb.org/3/tv/on_the_air?api_key=${process.env.API_KEY}`
+    const top_rated_url = `https://api.themoviedb.org/3/tv/top_rated?api_key=${process.env.API_KEY}`
+    const populartv_url = `https://api.themoviedb.org/3/tv/popular?api_key=${process.env.API_KEY}`
 
     const airingResponse = await axios.get(airing_today_url)
     const otairResponse = await axios.get(on_the_air_url)
@@ -24,28 +23,28 @@ router.get('/tvshows',async(req,res)=>{
 })
 
 router.get('/tvshows/airing_today/all',async(req,res)=>{
-    const airing_today_url = "https://api.themoviedb.org/3/tv/airing_today?api_key=3d812b53e8327e3496dbe05fb820cf0d"
+    const airing_today_url = `https://api.themoviedb.org/3/tv/airing_today?api_key=${process.env.API_KEY}`
     const airingResponse = await axios.get(airing_today_url)
 
     const title = "Airing Today TV Shows"
     res.render('all',{res: airingResponse.data.results, title})
 })
 router.get('/tvshows/on_the_air/all',async(req,res)=>{
-    const on_the_air_url = "https://api.themoviedb.org/3/tv/on_the_air?api_key=3d812b53e8327e3496dbe05fb820cf0d"
+    const on_the_air_url = `https://api.themoviedb.org/3/tv/on_the_air?api_key=${process.env.API_KEY}`
     const otairResponse = await axios.get(on_the_air_url)
 
     const title = "On The Air TV Shows"
     res.render('all',{res:otairResponse.data.results, title})
 })
 router.get('/tvshows/top_rated/all',async(req,res)=>{
-    const top_rated_url = "https://api.themoviedb.org/3/tv/top_rated?api_key=3d812b53e8327e3496dbe05fb820cf0d"
+    const top_rated_url = `https://api.themoviedb.org/3/tv/top_rated?api_key=${process.env.API_KEY}`
     const topResponse = await axios.get(top_rated_url)
 
     const title = "Top Rated TV Shows"
     res.render('all',{res:topResponse.data.results, title})
 })
 router.get('/tvshows/popular/all',async(req,res)=>{
-    const populartv_url = "https://api.themoviedb.org/3/tv/popular?api_key=3d812b53e8327e3496dbe05fb820cf0d"
+    const populartv_url = `https://api.themoviedb.org/3/tv/popular?api_key=${process.env.API_KEY}`
     const popularResponse = await axios.get(populartv_url)
 
     const title = "Popular TV Shows"
@@ -56,13 +55,13 @@ router.get('/tvshows/popular/all',async(req,res)=>{
 router.get('/tvshows/:id/details',async(req,res)=>{
     const {id} = req.params
 
-    const url = `https://api.themoviedb.org/3/tv/${id}?api_key=3d812b53e8327e3496dbe05fb820cf0d`;
+    const url = `https://api.themoviedb.org/3/tv/${id}?api_key=${process.env.API_KEY}`;
     const dr = await axios.get(url);
 
-    const casturl = `https://api.themoviedb.org/3/tv/${id}/credits?api_key=3d812b53e8327e3496dbe05fb820cf0d`;
+    const casturl = `https://api.themoviedb.org/3/tv/${id}/credits?api_key=${process.env.API_KEY}`;
     const cr = await axios.get(casturl);
 
-    const trailerUrl = `https://api.themoviedb.org/3/tv/${id}/videos?api_key=3d812b53e8327e3496dbe05fb820cf0d`;
+    const trailerUrl = `https://api.themoviedb.org/3/tv/${id}/videos?api_key=${process.env.API_KEY}`;
     const tr = await axios.get(trailerUrl);
 
     res.render('details',{cast:cr.data.cast, title:dr.data.original_title, overview:dr.data.overview , rating:dr.data.popularity , image:dr.data.poster_path , genres:dr.data.genres, trailer:tr.data.results})
